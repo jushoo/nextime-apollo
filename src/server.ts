9e0f8352599c4b44427955aae94057108e1a8b14
jushoo <denniel.joshua@pm.me>
2023-11-21 12:11:28 +0800

    Moved schema to a .graphql file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
-import {ApolloServer} from "@apollo/server";
-import {handlers, startServerAndCreateLambdaHandler} from "@as-integrations/aws-lambda";
+import {ApolloServer} from "@apollo/server"
+import {handlers, startServerAndCreateLambdaHandler} from "@as-integrations/aws-lambda"
+import { readFileSync } from 'fs'
 
-const typeDefs = `#graphql
-  type Query {
-    hello: String
-  }
-`
+const typeDefs = readFileSync('./src/schema/schema.graphql', { encoding: 'utf-8' })
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    books: () => []
   }
 }
 
